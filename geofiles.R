01121d07d1cb548922bdec58dd4993c41f2fba91
mar-garcia <mar.garcia.aloy@gmail.com>
2021-06-08 20:09:09 +0200

    Update geofiles.R

--- geofiles.R
+++ geofiles.R
@@ -16,10 +16,15 @@ geofiles_new$month <- substr(geofiles_new$file, 5, 6)
 geofiles_new$day <- substr(geofiles_new$file, 7, 8)
 geofiles_new$abr <- substr(geofiles_new$file, 10, 100)
 geofiles_new$abr <- gsub(".gpx", "", geofiles_new$abr)
-geofiles_new$type <- c(rep("walk",3), rep("others", 1),  rep("walk",5), rep("others",4), "walk", rep("others",3)) # walk - ski - others
-geofiles_new$subtype <- c(rep("run", 3), "bike",
-                          rep("hike", 2),  rep("run", 3), 
-                          "climb", "bike", "bike", "ferrata", "hike", "climb", rep("bike", 2))
+geofiles_new$type <- c("others", rep("walk",2), "ski", "walk", rep("others", 2), 
+                       rep("walk",3), "walk", rep("others",2), rep("walk",5), 
+                       "others", rep("walk",3)) # walk - ski - others
+# run - hike - climb - ferrata - 
+# bike - alpinism - scialpinismo - ski alpino - ski touring - trekking - walk
+geofiles_new$subtype <- c(
+  "climb", "hike", "run", "ski touring", "bike", "ski touring",
+  "walk", rep("bike", 2), "climb", "run", "bike", "climb", "hike", 
+  "walk", "run", "walk", "hike", "bike", "run", rep("hike", 2))
 geofiles_new$country <- "Italy"
 geofiles_new$region <- "Trentino"
 geofiles_new$tags <- "x"
@@ -27,7 +32,7 @@ for(i in 1:nrow(geofiles_new)){
   geofiles_new$marker_lat[i] <- mean(geodf$lat[geodf$file == geofiles_new$file[i]])
   geofiles_new$marker_lon[i] <- mean(geodf$lon[geodf$file == geofiles_new$file[i]])
   geofiles_new$marker_popup[i] <- geofiles_new$abr[i]
-  }
+}
 geofiles <- rbind(geofiles, geofiles_new)
 write.csv(geofiles, "geofiles.csv", row.names = F)
 
